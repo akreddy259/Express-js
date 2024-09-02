44d8434faa1289f17ed8a349993660a60cf2ed07
akreddy259 <126349519+akreddy259@users.noreply.github.com>
2024-09-02 18:07:02 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const path = require('path')
-
+const bycrpt = require('bcrypt')
 const app = express()
 app.use(express.json())
-
+const jwt = require('jsonwebtoken')
 const {open} = require('sqlite')
 const sqlite3 = require('sqlite3')
 
-const dbpath = path.join(__dirname, 'cricketTeam.db')
+const dbpath = path.join(__dirname, 'covid19IndiaPortal.db')
 
 let db = null
 
@@ -28,12 +28,145 @@ const InitializeDBAndServer = async () => {
 
 InitializeDBAndServer()
 
-app.get('/players/', async (request, response) => {
-  const getPlayersQuery = `
+const authentication = (request, response, next) => {
+  let jwtToken
+  const authheader = request.headers['authorization']
+  if (authheader !== undefined) {
+    jwtToken = authheader.split(' ')[1]
+  }
+  if (jwtToken === undefined) {
+    response.status(401)
+    response.send('Invalid JWT Token')
+  } else {
+    jwt.verify(jwtToken, 'MY_SECRET_TOKEN', (error, user) => {
+      if (error) {
+        response.status(401)
+        response.send('Invalid JWT Token')
+      } else {
+        next()
+      }
+    })
+  }
+}
+
+app.post('/login/', async (request, response) => {
+  const {username, password} = request.body
+  const getUserQuery = `
+    select * From user WHERE username='${username}';`
+  const userReponse = await db.get(getUserQuery)
+  if (userReponse === undefined) {
+    response.status(400)
+    response.send('Invalid User')
+  } else {
+    const isPasswordCorrect = await bycrpt.compare(
+      password,
+      userReponse.password,
+    )
+    if (isPasswordCorrect === true) {
+      const payload = {username: username}
+      const jwtToken = jwt.sign(payload, 'MY_SECRET_TOKEN')
+      response.send({jwtToken})
+    } else {
+      response.status(400)
+      response.send('Invalid password')
+    }
+  }
+})
+
+app.get('/states/', authentication, async (request, response) => {
+  const getStatesQuery = `
     select
         *
     From
-        cricket_team;`
-  const playersArray = await db.all(getPlayersQuery)
-  response.send(playersArray)
+        state;`
+  const statesArray = await db.all(getStatesQuery)
+  response.send(statesArray)
 })
+
+app.get('/states/:stateId/', authentication, async (request, response) => {
+  const {stateId} = request.params
+  const getStateQuery = `
+  select 
+    *
+  from
+      state
+  where
+   state_id = ${stateId};`
+  const state = await db.get(getStateQuery)
+  const {state_id, state_name, population} = state
+  const dbResponse = {
+    stateId: state_id,
+    stateName: state_name,
+    population: population,
+  }
+  response.send(dbResponse)
+})
+
+app.post('/districts/', authentication, async (request, response) => {
+  const getDetails = request.body
+  const {districtName, stateId, cases, cured, active, deaths} = getDetails
+  const addPlayerQuery = `
+  INSERT INTO 
+      district (district_name,state_id,cases,cured,active,deaths)
+  values(
+    '${districtName}',
+    ${stateId},
+    ${cases},
+    ${cured},
+    ${active},
+    ${deaths}
+  );`
+  const dbResponse = await db.run(addPlayerQuery)
+  response.send('District Successfully Added')
+})
+
+app.get(
+  '/districts/:districtId/',
+  authentication,
+  async (request, response) => {
+    const {districtId} = request.params
+    const getPlayerQuery = `
+  select 
+    *
+  from
+      district 
+  where
+   district_id = ${districtId};`
+    const district = await db.get(getPlayerQuery)
+    const {district_name, state_id, cases, cured, active, deaths} = district
+    const dbResponse = {
+      districtName: district_name,
+      stateId: state_id,
+      cases: cases,
+      cured: cured,
+      active: active,
+      deaths: deaths,
+    }
+    response.send(dbResponse)
+  },
+)
+
+app.put(
+  '/districts/:districtId/',
+  authentication,
+  async (request, response) => {
+    const {districtId} = request.params
+    const getbody = request.body
+    const {districtName, stateId, cases, cured, active, deaths} = getbody
+    const updateQuery = `
+        UPDATE district
+        SET 
+          district_name="${districtName}",
+          state_id=${stateId},
+          cases=${cases},
+          cured=${cured},
+          active= ${active},
+          deaths= ${deaths}
+          WHERE district_id=${districtId};`
+    await db.run(updateQuery)
+    response.send('District Details Updated')
+  },
+)
+
+
+
